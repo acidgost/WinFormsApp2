7dd0e3e2e468980a35fed5b782ffa1d7bb597924
ac1dg <ac1dg@DESKTOP-GAFMELE>
2023-12-08 15:04:13 +0300

    Создание основы

---
+++ WinFormsApp2/Core/HtmlLoader.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using AngleSharp.Html.Dom;
+
+namespace WinFormsApp2.Core
+{
+    class HtmlLoader
+    {
+        readonly HttpClient client;
+        readonly string url;
+
+        public HtmlLoader(IParserSettings settings)
+        {
+            client = new HttpClient();
+            url = $"{settings.BaseUrl}/{settings.Prefix}/";
+        }
+
+        public async Task<string> GetSourceByPageId(int id)
+        {
+            var currentUrl = url.Replace("{CurrentId}", id.ToString());
+            var response = await client.GetAsync(currentUrl);
+            string source = null;
+
+            if (response != null && response.StatusCode == HttpStatusCode.OK)
+            {
+                source = await response.Content.ReadAsStringAsync();
+            }
+
+            return source;
+        }
+    }
+}
---
+++ WinFormsApp2/Core/IParser.cs
@@ -0,0 +1,17 @@
+﻿using AngleSharp.Html.Dom;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinFormsApp2.Core
+{
+    internal interface IParser
+    {
+        interface IParser<T> where T : class
+        {
+            T Parse(IHtmlDocument document);
+        }
+    }
+}
